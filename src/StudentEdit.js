b54b5152d7caa08d69a76041507a4c5d96739301
garrettcobrien <gcobrien@purdue.edu>
2023-02-28 23:32:32 -0500

    working front end

---
+++ src/StudentEdit.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+
+class StudentEdit extends Component {
+
+    emptyItem = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    };
+
+    async componentDidMount() {
+        if (this.props.match.params.id !== 'new') {
+            const student = await (await fetch(`/students/${this.props.match.params.id}`)).json();
+            this.setState({item: student});
+        }
+    }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        const {item} = this.state;
+    
+        await fetch('/students' + (item.id ? '/' + item.id : ''), {
+            method: (item.id) ? 'PUT' : 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item),
+        });
+        this.props.history.push('/students');
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: this.emptyItem
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        let item = {...this.state.item};
+        item[name] = value;
+        this.setState({item});
+    }
+
+    render() {
+        const {item} = this.state;
+        const title = <h2>{item.id ? 'Edit Client' : 'Add Client'}</h2>;
+    
+        return <div>
+            
+            <Container>
+                {title}
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="firstName">first name</Label>
+                        <Input type="text" name="firstName" id="firstName" value={item.firstName || ''}
+                               onChange={this.handleChange} autoComplete="firstname"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="lastName">last name</Label>
+                        <Input type="text" name="lastName" id="lastName" value={item.lastName || ''}
+                               onChange={this.handleChange} autoComplete="lastname"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">email</Label>
+                        <Input type="text" name="email" id="email" value={item.email || ''}
+                               onChange={this.handleChange} autoComplete="email"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="lastName">Password</Label>
+                        <Input type="password" name="password" id="password" value={item.password || ''}
+                               onChange={this.handleChange} autoComplete="password"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>{' '}
+                        <Button color="secondary" tag={Link} to="/students">Cancel</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
+    }
+
+}
+export default withRouter(StudentEdit);
