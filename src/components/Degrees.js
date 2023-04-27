93adb9508befad77476ddf5a7e5a9dd2ad4b722b
Brandon Hart <brandonahart@yahoo.com>
2023-04-27 14:25:50 -0400

    Tutorial mode now completely functional

--- src/components/Degrees.js
+++ src/components/Degrees.js
@@ -1,11 +1,11 @@
 import { Component } from "react";
 import StudentService from "../services/StudentService";
 import APIService from "../services/APIService";
-import { Button, Form, FormGroup, Label } from "reactstrap";
-import { FormControl, InputLabel, MenuItem, Select, Typography } from "@mui/material";
+import { Form, FormGroup, Label } from "reactstrap";
+import { FormControl, Button, IconButton, InputLabel, MenuItem, Select, Table, TableBody, TableCell, TableRow, Typography } from "@mui/material";
 import { Container, ThemeProvider } from "@mui/system";
 import CssBaseline from "@mui/material/CssBaseline";
-
+import AddIcon from "@mui/icons-material/Add";
 import theme from "../theme";
 import { withRouter } from "react-router-dom";
 
@@ -35,10 +35,10 @@ class Degrees extends Component {
             this.setState({ student: res.data });
         });
         StudentService.getPlanofStudy(this.state.id).then(res => {
-            this.setState({planOfStudy: res.data})
+            this.setState({ planOfStudy: res.data })
         });
         APIService.getDepartments().then((res) => {
-            this.setState({departments: res.data});
+            this.setState({ departments: res.data });
         });
         this.forceUpdate();
     }
@@ -54,113 +54,131 @@ class Degrees extends Component {
 
     removeDegree(id, degree, text) {
         StudentService.changeDegree(id, degree, text).then((res) => {
-            this.setState({departments: res.data});
+            this.setState({ departments: res.data });
         });
         //this.forceUpdate();
     }
 
     handleChangeDep(e) {
-        this.setState({dep: e.target.value});
+        this.setState({ dep: e.target.value });
     }
     handleChangeType(e) {
-        this.setState({type: e.target.value});
+        this.setState({ type: e.target.value });
     }
     search(dep, type) {
         APIService.getDegreeList(dep, type).then((res) => {
-            this.setState({degrees: res.data});
+            this.setState({ degrees: res.data });
         })
     }
 
     render() {
-        const { student, type, dep, departments, degrees, planOfStudy} = this.state;
+        const { student, type, dep, departments, degrees, planOfStudy } = this.state;
         return (
             <div>
                 <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Container
-                          maxwidth="lg"
-                          alignItems="center"
-                          justifyContent="center"
-                          sx={{ padding: 10 }}>
-            <Typography
-                color="secondary"
-                variant="h3"
-                fontWeight={700}
-              >
-                Welcome to BoilerADVSR!
-              </Typography>
-              <Typography
-                color="text"
-                variant="h6"
-                fontWeight={400}
-              >
-                Next step let's add your majors, minors, or concentrations.
-              </Typography>
-            </Container>
-          <Container sx={{ padding: 0 }}>
-          <Typography
-                color="secondary"
-                variant="h5"
-                fontWeight={700}
-                style={{paddingBottom: 35}}
-              >
-                Search Degrees by Department and Type then Add to List Below
-              </Typography>
-              <Form >
-                    <FormControl fullWidth variant="filled">
-                        {/* Adding more degrees */}
-                        <InputLabel id="sort-label">Department</InputLabel>
-                        <Select labelId="sort-label" label="Department" value={dep} onChange={this.handleChangeDep}>
-                            {departments &&
-                                departments.map((department) => (
-                                    <MenuItem color="primary" key={department} value={department}> {department} </MenuItem>
-                                ))
-                            }
-                        </Select>
-                        <Select label="Please Select a Type of Degree" value={type} onChange={ (e) => this.handleChangeType(e)}>
-                            <MenuItem key="major" value="MAJOR">Major</MenuItem>
-                            <MenuItem key="minor" value="MINOR">Minor</MenuItem>
-                            <MenuItem key="concentration" value="CONCENTRATION">Concentration</MenuItem>
-                        </Select>
-                        <Button onClick={() => this.search(dep, type)}>Search</Button>
-                        {degrees &&
-                            degrees.map((degree) => (
-                                <label className="list-group-item" >
-                                    <Typography color="black">{degree.degreeTitle}</Typography>
-                                    <button label="add"  onClick={() => this.removeDegree(this.state.id, degree.degreeTitle, "true")}>add</button>
-                                </label>
-                            ))
-                        }
-                    </FormControl>
-                </Form>
-                <br></br>
-                <Form>
-                    <Typography color="secondary" variant="h4">Degrees added below</Typography>
-                    <FormGroup>
-                        {/* Getting degrees */}
-                       {planOfStudy.degrees?.map((degree) => (
-                        <div>
-                            <Label for="degree">
-                                <Typography focused color="secondary">{degree.degreeTitle}</Typography>
-                            </Label> {degree.degreeType}
-                            <Button type="submit" onClick={ () => this.removeDegree(this.state.id, degree.degreeTitle, "false")}>Remove</Button>
-                        </div>
-                       ))} 
-                       <Button
-                            style={{ marginTop: 10 }}
-                            variant="contained"
+                    <CssBaseline />
+                    <Container
+                        maxwidth="lg"
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{ padding: 10 }}>
+                        <Typography
                             color="secondary"
-                            type="button"
-                            onClick={() => this.toAddSemester(this.state.id, "", "")}                  
+                            variant="h3"
+                            fontWeight={700}
+                        >
+                            Welcome to BoilerADVSR!
+                        </Typography>
+                        <Typography
+                            color="text"
+                            variant="h6"
+                            fontWeight={400}
+                        >
+                            Next step let's add your majors, minors, or concentrations.
+                        </Typography>
+                    </Container>
+                    <Container sx={{ padding: 0 }}>
+                        <Typography
+                            color="secondary"
+                            variant="h5"
+                            fontWeight={700}
+                            style={{ paddingBottom: 35 }}
+                        >
+                            Search Degrees by Department and Type then Add to List Below
+                        </Typography>
+                        <Form >
+                            <Typography variant="h4" color="secondary" >Search Degrees to add by Department and Type</Typography>
+                            <FormGroup>
+                                {/* Adding more degrees */}
+                                <Select id="category" style={{ backgroundColor: "gray", marginTop: 10 }} displayEmpty label="Please Select a Type of Department" value={dep} onChange={this.handleChangeDep}>
+                                    <MenuItem value="">Department</MenuItem>
+                                    {departments &&
+                                        departments.map((department) => (
+                                            <MenuItem color="primary" key={department} value={department}> {department} </MenuItem>
+                                        ))
+                                    }
+                                </Select>
+                                <Select sx={{ marginLeft: 5 }} label="Please Select a Type of Degree" displayEmpty style={{ backgroundColor: "gray" }} value={type} onChange={(e) => this.handleChangeType(e)}>
+                                    <MenuItem value="">Type</MenuItem>
+                                    <MenuItem key="major" value="MAJOR">Major</MenuItem>
+                                    <MenuItem key="minor" value="MINOR">Minor</MenuItem>
+                                    <MenuItem key="concentration" value="CONCENTRATION">Concentration</MenuItem>
+                                </Select>
+                                <Button variant="contained" color="secondary" type="button" sx={{ marginLeft: 5, fontSize: 20 }} onClick={() => this.search(dep, type)}>Search</Button>
+                                <Table style={{ maxWidth: "400px" }}>
+                                    <TableBody>
+                                        {degrees &&
+                                            degrees.map((degree) => (
+                                                <TableRow>
+                                                    <TableCell>
+                                                        {degree.degreeTitle} {" "}
+                                                        <IconButton
+                                                            variant="contained"
+                                                            color="secondary"
+                                                            size="small"
+                                                            type="submit"
+                                                            onClick={() => this.removeDegree(this.state.id, degree.degreeTitle, "true")}
+                                                        >
+                                                            <AddIcon />
+                                                        </IconButton>
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))
+                                        }
+                                    </TableBody>
+                                </Table>
+                            </FormGroup>
+                        </Form>
+                        <br></br>
+                        <Form>
+                            <Typography variant="h4" color="secondary" sx={{marginTop: 5}}>Degrees Added Below</Typography>
+                            <Table style={{maxWidth: "400px"}}>
+                                <TableBody>
+                                {/* Getting degrees */}
+                                {planOfStudy.degrees?.map((degree) => (
+                                    <TableRow>
+                                        <TableCell>
+                                            {degree.degreeTitle}{" "}
+                                            <Button color="secondary" type="submit" onClick={() => this.removeDegree(this.state.id, degree.degreeTitle, "false")}>Remove</Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                                </TableBody>
+                            </Table>
+                            <Button
+                                style={{ marginTop: 10 }}
+                                variant="contained"
+                                color="secondary"
+                                type="button"
+                                onClick={() => this.toAddSemester(this.state.id, "", "")}
                             >
-                            <Typography color="primary">Next Step</Typography>
-                        </Button>{" "}
-                    </FormGroup>
-                </Form>        
-          </Container>
-        </ThemeProvider>
+                                <Typography color="primary">Next Step</Typography>
+                            </Button>{" "}
+                        </Form>
+                    </Container>
+                </ThemeProvider>
 
-        </div>
+            </div>
         );
     }
 }
